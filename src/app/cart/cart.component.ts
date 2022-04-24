4a4cb8e866fdb877419505b6d26ef5e3e4c58a6b
rpviper <x6x6x61982@hotmail.com>
2022-04-24 17:17:31 +0300

    totals

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CartComponent implements OnInit {
   cartProducts: any[] = [];
+  totalSum = 0;
+  totalItems = 0;
 
   constructor() { }
 
@@ -14,19 +16,23 @@ export class CartComponent implements OnInit {
     const cartItemsSS = sessionStorage.getItem("cartItems");
     if (cartItemsSS) {
       this.cartProducts = JSON.parse(cartItemsSS);
+      this.cartTotalSum();
     }
+
   }
   decreaseQuantity(cartProduct: any) {
     cartProduct.quantity--;
     if (cartProduct.quantity <= 0) {    // see on selle jaoks et ei läheks miinusesse ostukorv
       this.removeProduct(cartProduct);
-    }
+ }
     sessionStorage.setItem("cartItems", JSON.stringify(this.cartProducts));
+    this.cartTotalSum();
   }
   
   increaseQuantity(cartProduct: any) {
-    cartProduct.quantity++;
+    cartProduct.quantity++; 
     sessionStorage.setItem("cartItems", JSON.stringify(this.cartProducts));
+    this.cartTotalSum();
   }
   
   removeProduct(cartProduct: any) {
@@ -34,11 +40,29 @@ export class CartComponent implements OnInit {
     if (index >= 0) {
     this.cartProducts.splice(index, 1);
     sessionStorage.setItem("cartItems", JSON.stringify(this.cartProducts));
+    this.cartTotalSum();
     }
   }
 
+  private cartTotalSum() {
+    this.totalSum = 0;
+    this.cartProducts.forEach(element => this.totalSum = this.totalSum + element.product.price * element.quantity);
+    this.totalSum = Math.round((this.totalSum + Number.EPSILON) * 100) / 100   // this will round off to 2 decimals
+    }
 
-}
+  emptyCart() {
+    this.cartProducts = [];
+    sessionStorage.setItem("cartItems", JSON.stringify(this.cartProducts));
+    this.cartTotalSum();
+  }
+
+  toPay() {
+    
+  }
+
+  }
+
+
 
 // ISESEISVALT: ostukorvi kogusumma
   // kui võtate hinna, siis peab olema element.product.price
@@ -50,3 +74,4 @@ export class CartComponent implements OnInit {
 
   // *ngIf'ga ära kustutamine kui on 0 toodet 
   // MAKSMISE NUPP ja TÜHJENDA NUPP
+
